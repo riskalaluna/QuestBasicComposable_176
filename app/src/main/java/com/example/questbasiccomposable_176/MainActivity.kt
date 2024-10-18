a1f7e1e2084a31248915bf0279e842b30d2fc533
riskalaluna <riskalaluna@gmail.com>
2024-10-18 18:34:09 +0700

    menampilkan teks "Ini adalah halaman login" dengan ukuran 40 sp dan gaya font italic

--- app/src/main/java/com/example/questbasiccomposable_176/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_176/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -50,6 +51,9 @@ fun BasicColumn(modifier: Modifier = Modifier) {
             fontWeight = FontWeight.Bold
         )
         Spacer(modifier = Modifier.padding(10.dp))
+        Text("Ini adalah halaman login", fontSize = 40.sp,
+            fontStyle = FontStyle.Italic
+        )
 
     }
 }
