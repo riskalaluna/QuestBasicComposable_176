d856dae7321ff1a5d29495e1005ccff62f6f232f
riskalaluna <riskalaluna@gmail.com>
2024-10-18 18:42:32 +0700

    Menambahkan logo umy tercinta

--- app/src/main/java/com/example/questbasiccomposable_176/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_176/MainActivity.kt
@@ -4,16 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -54,7 +58,12 @@ fun BasicColumn(modifier: Modifier = Modifier) {
         Text("Ini adalah halaman login", fontSize = 20.sp,
             fontStyle = FontStyle.Italic
         )
+        Spacer(modifier = Modifier.padding(20.dp))
 
+        Image(painter = painterResource(id = R.drawable.umy),
+            contentDescription = "",
+            modifier = Modifier.clip(CircleShape)
+        )
     }
 }
 
